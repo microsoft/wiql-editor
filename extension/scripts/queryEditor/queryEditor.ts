409f44b5251b1fc970c4c5c7b1771755c679de8f
AminTi <titiamin@icloud.com>
2023-09-07 13:26:25 +0200

    test new bord

--- extension/scripts/queryEditor/queryEditor.ts
+++ extension/scripts/queryEditor/queryEditor.ts
@@ -41,6 +41,7 @@ async function saveQuery(): Promise<string | null> {
         path: configuration.query.path,
         name: configuration.query.name,
     };
+    console.log("queryItem", queryItem)
     trackEvent("SaveQuery", {wiqlLength: "" + editor.getValue().length, isNew: "" + !configuration.query.id});
     if (configuration.query.id && configuration.query.id !== "00000000-0000-0000-0000-000000000000") {
         const updated = await getWitClient().updateQuery(queryItem, context.project.name, configuration.query.id);
@@ -59,6 +60,6 @@ async function saveQuery(): Promise<string | null> {
     return null;
 }
 const callbacks: ICallbacks = {
-    okCallback: saveQuery(),
+    okCallback: saveQuery,
 };
 configuration.loaded(callbacks);
