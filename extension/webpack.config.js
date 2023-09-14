45857771db8a5bf85ee0b71f38b37cb47b60d0f9
AminTi <titiamin@icloud.com>
2023-09-14 15:03:38 +0200

    adding webpacking

--- extension/webpack.config.js
+++ extension/webpack.config.js
@@ -1,45 +1,81 @@
-const webpack = require('webpack');
 const path = require("path");
-const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
+const webpack = require("webpack");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-    // Don't want symbols.ts to be minified - renaming the class names 
-    mode: "development",
-    entry: {
-        playground: "./scripts/wiqlPlayground/playground.ts",
-        queryContext: "./scripts/queryContext/queryContext.ts",
-        queryEditor: "./scripts/queryEditor/queryEditor.ts"
-    },
-    output: {
-        libraryTarget: "amd",
+  target: "web",
+  entry: {
+    playground: "./scripts/wiqlPlayground/playground.ts",
+    queryContext: "./scripts/queryContext/queryContext.ts",
+    queryEditor: "./scripts/queryEditor/queryEditor.ts"
+    
+  },
+  output: {
+    libraryTarget: "amd",
         path: path.resolve(__dirname, "dist"),
-        filename: "[name].js"
-    },
-    externals: [{
-        "q": true,
-        "react": true,
-        "react-dom": true,
-        "monaco": true,
+        filename: "[name].js",
+        publicPath: "./dist",
+  },
+  devtool: "inline-source-map",
+  externals: [{
+    "q": true,
+    "react": true,
+    "react-dom": true,
+    "monaco": true,
     },
         /^TFS\//, // Ignore TFS/* since they are coming from VSTS host 
         /^VSS\//  // Ignore VSS/* since they are coming from VSTS host
     ],
-    resolve: {
-        // alias: { "office-ui-fabric-react": path.join(process.cwd(), 'node_modules', 'office-ui-fabric-react', 'lib-amd') },
-        extensions: [".ts", ".tsx", ".js"],
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
+    alias: {
+      "VSSUI": path.resolve(__dirname, "node_modules/azure-devops-ui"),
+      "VSS": path.resolve(__dirname, "node_modules/vss-web-extension-sdk"),
     },
-    plugins: [
-      new BundleAnalyzerPlugin({
-        openAnalyzer: false,
-        reportFilename: "bundle-analysis.html",
-        analyzerMode: "static"
-      })
-    ],
-    devtool: "source-map",
-    module: {
-      rules: [
-        // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
-        { test: /\.tsx?$/, loader: "ts-loader" }
+    modules: [path.resolve("."), "node_modules"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: "tslint-loader",
+        enforce: "pre",
+        options: {
+            emitErrors: true,
+            failOnHint: true
+        }
+    },
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader"
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          "style-loader",
+          "css-loader",
+          "sass-loader"
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(png|svg|jpg|gif|html)$/,
+        use: "file-loader"
+      }
+    ]
+  },
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: "**/*.html", to: "./",  },
+        { from: "**/*.png", to: "./img", context: "img" },
+        { from: "./styles", to: "./styles", context: "./" },
+        { from: "./azure-devops-extension.json", to: "./azure-devops-extension.json" },
+        { from: "./node_modules/vss-web-extension-sdk/lib/VSS.SDK.min.js", to: "./" },
       ]
-    }
+    })
+  ]
 };
