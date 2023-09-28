896694be66d3eb9eefccb874ee9ec54101a5f76c
AminTi <titiamin@icloud.com>
2023-09-28 11:38:55 +0200

    sv

--- extension/scripts/queryEditor/queryEditor.ts
+++ extension/scripts/queryEditor/queryEditor.ts
@@ -7,6 +7,7 @@ import { trackEvent } from "../events";
 import { ICallbacks, IContextOptions } from "../queryContext/contextContracts";
 import { setupEditor } from "../wiqlEditor/wiqlEditor";
 
+console.log("test")
 trackEvent("pageLoad");
 const configuration: IContextOptions = VSS.getConfiguration();
 const target = document.getElementById("wiql-box");
