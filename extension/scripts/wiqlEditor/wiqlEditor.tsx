2ffe5961ab6d11360452299a1406687aafc66561
AminTi <titiamin@icloud.com>
2023-11-27 20:07:28 +0100

    import scss

--- extension/scripts/wiqlEditor/wiqlEditor.tsx
+++ extension/scripts/wiqlEditor/wiqlEditor.tsx
@@ -13,6 +13,7 @@ import { exportWiq, importWiq, saveQuery } from "./importExport";
 import * as Wiql from "./wiqlDefinition";
 import * as monaco from 'monaco-editor';
 import { getProject } from "../getProject";
+import '../../../extension/styles/wiqlEditor.scss';
 
 
 
