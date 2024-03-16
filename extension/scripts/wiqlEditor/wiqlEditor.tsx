740d544a7c6f0ae846a889aa82506ac904563ef6
AminTi <titiamin@icloud.com>
2024-03-16 13:24:52 +0100

    resolve Dark mode issue

--- extension/scripts/wiqlEditor/wiqlEditor.tsx
+++ extension/scripts/wiqlEditor/wiqlEditor.tsx
@@ -82,8 +82,11 @@ function renderToolbar(isPanel: boolean, callback: () => void) {
         , elem, callback);
 }
 
+
 export function setupEditor(target: HTMLElement, onChange?: (errorCount: number) => void, intialValue?: string, queryName?: string, configuration?: any, isPanel?: boolean): monaco.editor.IStandaloneCodeEditor {
     //set isPanel default value to false if not provided
+    
+    
     renderToolbar(isPanel, async () => {
         if (queryName) {
             return;
@@ -102,7 +105,9 @@ export function setupEditor(target: HTMLElement, onChange?: (errorCount: number)
     monaco.languages.onLanguage(Wiql.def.id, () => {
         monaco.languages.setMonarchTokensProvider(Wiql.def.id, Wiql.language);
         monaco.languages.setLanguageConfiguration(Wiql.def.id, Wiql.conf);
+      
     });
+
     const defaultVal =
         `SELECT
         [System.Id],
@@ -117,15 +122,7 @@ WHERE
 ORDER BY [System.ChangedDate] DESC
 
 `;
-
-
-
-
-
-
-
-
-    const editor = monaco.editor.create(target, {
+  const editor = monaco.editor.create(target, {
         language: Wiql.def.id,
         value: intialValue || defaultVal,
         automaticLayout: true,
@@ -136,7 +133,7 @@ ORDER BY [System.ChangedDate] DESC
         minimap: { enabled: false }, // Disable the minimap
     });
 
-    format(editor);
+   format(editor);
     editor.addAction({
         id: "format",
         contextMenuGroupId: "1_modification",
@@ -147,11 +144,14 @@ ORDER BY [System.ChangedDate] DESC
         ],
         run: () => { format(editor); return null as any; },
     });
+
+
     $(".wiq-input").change(() => importWiq(editor));
     $(".wiq-export").click(() => exportWiq(editor, queryName));
     $("#saveQueryBtn").click(() => saveQuery(editor, configuration));
     monaco.languages.registerHoverProvider(Wiql.def.id, getHoverProvider());
     monaco.languages.registerCompletionItemProvider(Wiql.def.id, completionProvider);
+
 
     const model = editor.getModel();
     const errorChecker = new ErrorChecker();
